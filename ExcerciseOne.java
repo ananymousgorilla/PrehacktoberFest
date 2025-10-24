4747c99310fe7ed9e1227ee835859a3ae0a6dd14
codeseeker <secondarycode230651@gmail.com>
2025-10-24 07:59:21 +0500

    ADded third feature

--- ExcerciseOne.java
+++ ExcerciseOne.java
@@ -5,6 +5,7 @@ public class ExerciseOne
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
-		
+		System.out.println("Addded a new feature!!");
+		System.out.println("WohoooooooOOO");
 	}
 }
